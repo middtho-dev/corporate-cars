6251d91ead157acec883b8301cc8f47cb368da05
middtho-dev <middtho@yandex.ru>
2025-04-28 09:40:19 +0300

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app dark">
+    <h1>Календарь бронирования</h1>
+    <Calendar />
+  </div>
+</template>
+
+<script setup>
+import Calendar from './components/Calendar.vue'
+</script>
+
+<style>
+body {
+  margin: 0;
+  background: #121212;
+  color: white;
+  font-family: Arial, sans-serif;
+}
+.app {
+  padding: 20px;
+}
+</style>
